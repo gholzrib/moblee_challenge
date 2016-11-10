e7f9ac57c438abc1cecfe5f9c150a4491f2fb5cf
gholzrib <guntherhr@gmail.com>
2016-11-10 21:44:08 -0200

    Project and package structure created.

---
+++ app/src/main/java/gholzrib/mobleechallenge/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package gholzrib.mobleechallenge.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gholzrib.mobleechallenge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
